c28f78e0f298a8be4d25e8e77ec9bd48b86722b9
suraj <surajsht2055@gmail.com>
2023-07-24 20:34:28 +0545

    navbar design complete for desktop view

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -2,22 +2,35 @@ import NavbarItem from "../../Data";
 
 const Navbar = () => {
   return (
-    <div className="container">
-      <div>
-        Wander<span>Sage</span>
+    <div className="container absolute left-0 right-0 top-9 flex items-center justify-between">
+      <div className="cursor-pointer text-2xl	font-bold">
+        Wander<span className="text-brand">Sage</span>
       </div>
 
-      <div>
-        <ul>
+      <div className="flex items-center space-x-[36px]">
+        <ul className="flex items-center space-x-[24px]">
           {NavbarItem.NavbarItem.map((data) => {
             let { id, item } = data;
 
-            return <li key={id}>{item}</li>;
+            return (
+              <li
+                key={id}
+                className="cursor-pointer text-[#929292] first:text-brand"
+              >
+                {item}
+              </li>
+            );
           })}
         </ul>
 
-        <button> Sign in </button>
-        <button> Sign up </button>
+        <div className="flex items-center space-x-[16px]">
+          <button className="cursor-pointer rounded-lg border-2 border-brand px-[24px] py-[8px]">
+            Sign in
+          </button>
+          <button className="cursor-pointer rounded-lg bg-brand px-[24px] py-[8px] text-white">
+            Sign up
+          </button>
+        </div>
       </div>
     </div>
   );
