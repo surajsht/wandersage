94f3fa5c6c214f6cfcf6477463de01f131776acf
suraj <surajsht2055@gmail.com>
2023-07-26 21:56:38 +0545

    popular destination section design completed

--- src/app/components/popularDestination/PopularDestination.jsx
+++ src/app/components/popularDestination/PopularDestination.jsx
@@ -1,28 +1,35 @@
 import SectionTitle from "../sectionTitle/SectionTitle";
 import { PopularDestinationData } from "../../Data";
 import Image from "next/image";
+import styles from "./popularDestination.module.css";
+import { MdLocationOn } from "react-icons/md";
 
 const PopularDestination = () => {
   return (
-    <div className="container">
+    <div className="container mb-12 lg:mb-16">
       <SectionTitle
         title="Popular Destinations"
         description=" Get ready to embark on an extraordinary journey with our selection of popular destinations that are sure to captivate your senses and ignite your wanderlust."
       />
 
-      <div className="">
+      <div className={styles.popularDestination}>
         {PopularDestinationData.map((item) => {
           let { id, image, title } = item;
 
           return (
-            <div key={id} className="relative mb-4">
-              <div className="absolute inset-0 z-10 h-full w-full bg-gradient-to-t from-black to-transparent"></div>
-              <div className="relative h-60 w-full">
-                <Image src={image} fill={true} className="object-cover" />
+            <div key={id} className="relative mb-4 md:mb-0">
+              <div className="absolute inset-0 z-10 h-full w-full rounded-3xl bg-gradient-to-t from-black to-transparent"></div>
+              <div className={`relative h-60 w-full ${styles.popularImage}`}>
+                <Image
+                  src={image}
+                  fill={true}
+                  className="rounded-3xl object-cover"
+                />
               </div>
-              <h2 className="absolute bottom-4 left-4 z-20 rounded-lg bg-white px-6 py-1 text-black">
-                {title}
-              </h2>
+              <div className="absolute bottom-4 left-4 z-20 flex items-center gap-1 rounded-lg bg-white px-6 py-1 text-black">
+                <MdLocationOn className="h-5 w-5 text-[#747474]" />
+                <h2 className="font-bold">{title}</h2>
+              </div>
             </div>
           );
         })}
