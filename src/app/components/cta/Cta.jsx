fa7b4b55105cd5aca91c3abd5696515a649655e7
suraj <surajsht2055@gmail.com>
2023-07-30 20:27:55 +0545

    cta section design complete

--- src/app/components/cta/Cta.jsx
+++ src/app/components/cta/Cta.jsx
@@ -1,13 +1,16 @@
 const Cta = () => {
   return (
-    <div className="container mb-12 flex flex-col items-center justify-center bg-brand py-6 text-center lg:mb-16">
-      <h2 className="mb-6 font-secondary text-[32px] leading-[44.8px] text-white">
-        Subscribe newsletter & get latest news
+    <div className="container mb-12 flex flex-col items-center justify-center bg-brand py-6 text-center md:px-6 lg:mb-16 lg:py-16">
+      <h2 className="md:leading[57.4px] mb-6 font-secondary text-[32px] font-bold leading-[44.8px] text-white md:text-[41px] xl:text-[51px] xl:leading-[71.4px]">
+        Subscribe newsletter & get latest news and updates
       </h2>
 
       <div className="flex items-center gap-2">
-        <input type="text" className="rounded-sm p-2" />
-        <button className="flex items-center justify-center rounded-sm bg-white p-2 px-4 text-base font-semibold leading-[22.4px]">
+        <input
+          type="text"
+          className="rounded-sm p-2 md:w-[280px] xl:h-[58px] xl:w-[360px] xl:rounded-lg"
+        />
+        <button className="flex items-center justify-center rounded-sm bg-white p-2 px-4 text-base font-semibold leading-[22.4px] xl:h-[58px] xl:rounded-lg">
           Subscribe
         </button>
       </div>
